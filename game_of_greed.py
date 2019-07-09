c597ba38d5a8272cc768ad412712bb528ee0d8f3
tammytdo11 <tammytdo11@gmail.com>
2019-07-09 00:12:25 -0700

    welcome message, dice roll, score keeping, and function structure.

--- game_of_greed.py
+++ game_of_greed.py
@@ -5,6 +5,8 @@ Game Of Greed
 """
 
 import random
+import time
+
 min = 1
 max = 6
 overall_score = 0
@@ -13,39 +15,58 @@ num_dice = 6
 dice_rolled = []
 dice_kept_this_round = []
 
-point_guide = {
-    '5': 50,
-    '1': 100
-}
+# players will be self-scoring
+# point_guide = {
+#     '5': 50,
+#     '1': 100
+# }
 
 actions_dict = {'b': 'Bank Points',
            'r': 'Roll Dice',
            'q': 'Quit Game'
           }
 
-for i in range(num_dice):
-    dice_rolled.append(random.randint(min, max))
+def welcome():
+    print("Welcome to Game of Greed.")
+    time.sleep(1)
+    print("Enter any lucky number to roll the dice")
+    input() #why does this only work when I enter integers?
 
-action_selection = """
-Options: 
-Enter at least one dice number to roll again.
-Enter 'b' to bank points.
-Enter 'q' to quit the game:
-            """
+def rolling_dice():
+    print("\nrolling dice...")
+    time.sleep(1)
 
-print('Random dice rolled: ', dice_rolled)
-# print(f'Random dice rolled: {dice_rolled}')
-# why doesn't this f string work?
+def dice_done_rolling():
+    for i in range(num_dice):
+       dice_rolled.append(random.randint(min, max))
+    print('You rolled: ', dice_rolled)
+    # print(f'Random dice rolled: {dice_rolled}')
+    # why doesn't this f string work?
+    
+    dice_kept_this_round = str(input("Which would you like to keep? "))
+    dice_kept_this_round = [int(num) for num in dice_kept_this_round]
+    print('You kept: ', dice_kept_this_round)
+
+def enter_score(score_this_round):
+    enter_points = int(input("Enter score for last round: "))
+    score_this_round += enter_points
+    print(score_this_round)
+
+
 
 def prompt_after_roll():
-    print(action_selection)
+    print("""Options: 
+            Enter at least one dice number to roll again.
+            Enter 'b' to bank points.
+            Enter 'q' to quit the game:
+                    """)
 
 def selection_after_prompt():
     player_selection = str(input()).lower()
 
-    # if player_selection == THE DICE NUMBERS:
+    if player_selection == 'THE DICE NUMBERS':
         for i in player_selection.split():
-            dice_kept_this_round.append(i):
+            dice_kept_this_round.append(i)
             
 
     elif player_selection == 'b':
@@ -69,6 +90,11 @@ def selection_after_prompt():
     return player_selection
 
 #Fuction Calls
+welcome()
+rolling_dice()
+dice_done_rolling()
+enter_score(score_this_round)
+
 prompt_after_roll()
 selection_after_prompt()
 
@@ -78,7 +104,9 @@ selection_after_prompt()
 
 
 """
-Add to points_guide once I understand how to calculate:
+Add to points_guide once I understand how to calculate.
+Try using sets. if 5, 5, 5 is a set of rolled_dice.
+Just reviewed class recording. User will self calulate. 
 
     #'three of a kind': 100 * times the number rolled,
     #'except for three ones': 1000,
