bb23675083a50fb145ccda290883261c62047f58
tammytdo11 <tammytdo11@gmail.com>
2019-07-19 12:55:16 -0700

    increment turn values and validated dice selection

--- game_of_greed.py
+++ game_of_greed.py
@@ -9,56 +9,65 @@ import time
 
 # initialized values
 max_turns = 3
-min_dice_val = 1
-max_dice_val = 6
+min_die_val = 1
+max_die_val = 6
+
+#BUG GAME TURNS ARE DOUBLE INCREMENTING WHEN BANKING AND WHEN NUM DICE TO ROLL = 0.
 
 def welcome():
-    print("Welcome to Game of Greed\n")
+    print("Welcome to Game of Greed")
     time.sleep(.3)
 
 def start_turn():
-    current_turn = 0
+    current_turn = 1
     money_pot = 0
     bank = 0
 
-    while current_turn < max_turns:
-        num_dice_to_roll = 6
+    while current_turn < max_turns + 1:
+        num_die_to_roll = 6
 
-        while num_dice_to_roll > 0:
+        while num_die_to_roll > 0:
             print('\n***********************')
-            print(f'BANK VALUE: {bank}')
-            print(f'MONEY POT VALUE: {money_pot}')
-            print(f'CURRENT TURN: {current_turn}.')
+            print(f'BANK VALUE        {bank}')
+            print(f'MONEY POT VALUE   {money_pot}')
+            print(f'CURRENT TURN      {current_turn}/{max_turns}.')
             print('***********************\n')
 
             time.sleep(.3)
-            print("rolling dice...\n")
+            print("rolling die...\n")
             time.sleep(.3)
 
             # go to do_round function
-            num_dice_to_roll, money_pot, bank, current_turn = do_round(num_dice_to_roll, money_pot, bank, current_turn)
+            num_die_to_roll, money_pot, bank, current_turn = do_round(num_die_to_roll, money_pot, bank, current_turn)
         
+        if num_die_to_roll == 0:
+            # user_input_to_bank = input("Out of die. Enter b to bank: ")
 
-        if num_dice_to_roll == 0:
-            # user_input_to_bank = input("Out of dice. Enter b to bank: ")
-           
-            print('Points banked and dice reset.\n')
+            print('All die used\n')
+            print('Points banked and die reset\n')
 
+            time.sleep(.5)
+            current_turn += 1
+            print('New turn started\n')
+            
             money_pot, bank = bank_round(money_pot, bank)
 
-    current_turn += 1
     print("Out of turns.\n")
     print(f'You scored {bank} points!')
     quit()
 
 
-def do_round(num_dice_to_roll, money_pot, bank, current_turn):
-    dice_rolled = []
+def do_round(num_die_to_roll, money_pot, bank, current_turn):
+    die_rolled = []
 
-    for i in range(num_dice_to_roll):
-       dice_rolled.append(random.randint(min_dice_val, max_dice_val))
-    print(f'You rolled: {dice_rolled}\n')   
-    user_selection = str(input("Enter dice to keep or enter 'b' to bank: "))
+    for i in range(num_die_to_roll):
+       die_rolled.append(random.randint(min_die_val, max_die_val))
+    
+    print(f'You rolled: {die_rolled}\n')   
+    user_selection = str(input("Enter at least one die to roll again, or enter 'b' to bank points: "))
+    
+    #check if each number in input.split() is in die_rolled.
+    #if in die_rolled, use index() of the input to find it's index in die_rolled. pop the num at that index out. add it to a new array. find out if zilch, single, double, triple., etc.
     
     # Go to make_choice function
     user_selection, bank, money_pot = make_choice(user_selection, bank, money_pot)
@@ -66,12 +75,30 @@ def do_round(num_dice_to_roll, money_pot, bank, current_turn):
     if user_selection == 'b':
         current_turn += 1
         money_pot = 0
-        num_dice_to_roll = 0
-        return num_dice_to_roll, money_pot, bank, current_turn
+        num_die_to_roll = 0
+        return num_die_to_roll, money_pot, bank, current_turn
     
-    user_selection = [char for char in user_selection]
-    num_dice_to_roll -= len(user_selection)
-    return num_dice_to_roll, money_pot, bank, current_turn
+    user_selection = [int(char) for char in user_selection]
+    
+    user_selection, die_rolled = remove_from_die_rolled(user_selection, die_rolled)
+    
+    num_die_to_roll -= len(user_selection)
+    return num_die_to_roll, money_pot, bank, current_turn
+
+def remove_from_die_rolled(user_selection, die_rolled):
+    for i in user_selection:
+        # keep for debugging elif statement
+        # print("i is", i, "and type is", type(i))
+
+        if i in user_selection:
+            die_rolled.remove(i)
+            print(f'Dice #{i} kept')
+
+        # Why won't this message print when the dice value i is not present in user_selection?  
+        elif i not in die_rolled:
+            print('Invalid die selection.')
+
+    return user_selection, die_rolled
 
 def make_choice(user_selection, bank, money_pot):
     
