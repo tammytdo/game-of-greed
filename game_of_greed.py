914c502fc92396e2396e2bf3a935898b174f5b48
tammytdo11 <tammytdo11@gmail.com>
2019-07-19 20:07:27 -0700

    refactored big fuction into small functions

--- game_of_greed.py
+++ game_of_greed.py
@@ -12,8 +12,6 @@ max_turns = 3
 min_die_val = 1
 max_die_val = 6
 
-#BUG GAME TURNS ARE DOUBLE INCREMENTING WHEN BANKING AND WHEN NUM DICE TO ROLL = 0.
-
 def welcome():
     print("Welcome to Game of Greed")
     time.sleep(.3)
@@ -27,59 +25,36 @@ def start_turn():
         num_die_to_roll = 6
 
         while num_die_to_roll > 0:
-            print('\n***********************')
-            print(f'BANK VALUE        {bank}')
-            print(f'MONEY POT VALUE   {money_pot}')
-            print(f'CURRENT TURN      {current_turn}/{max_turns}.')
-            print('***********************\n')
+            game_summary(bank, money_pot, current_turn, max_turns)
 
-            time.sleep(.3)
-            print("rolling die...\n")
-            time.sleep(.3)
+            rolling_die_text()
 
-            # go to do_round function
             num_die_to_roll, money_pot, bank, current_turn = do_round(num_die_to_roll, money_pot, bank, current_turn)
         
         if num_die_to_roll == 0:
-            # user_input_to_bank = input("Out of die. Enter b to bank: ")
+            money_pot, bank, current_turn = bank_and_reset_die(money_pot, bank, current_turn)
 
-            print('All die used\n')
-            print('Points banked and die reset\n')
-
-            time.sleep(.5)
-            current_turn += 1
-            print('New turn started\n')
-            
-            money_pot, bank = bank_round(money_pot, bank)
-
-    print("Out of turns.\n")
-    print(f'You scored {bank} points!')
-    quit()
-
+    end_game(bank)
 
 def do_round(num_die_to_roll, money_pot, bank, current_turn):
     die_rolled = []
 
-    for i in range(num_die_to_roll):
-       die_rolled.append(random.randint(min_die_val, max_die_val))
-    
-    print(f'You rolled: {die_rolled}\n')   
+    die_rolled, num_die_to_roll = determine_which_die_rolled(die_rolled, num_die_to_roll)
+
+    # handle error for entries that are not a number or the letter b
     user_selection = str(input("Enter at least one die to roll again, or enter 'b' to bank points: "))
     
-    #check if each number in input.split() is in die_rolled.
-    #if in die_rolled, use index() of the input to find it's index in die_rolled. pop the num at that index out. add it to a new array. find out if zilch, single, double, triple., etc.
+    #find out if zilch, single, double, triple., etc.
     
-    # Go to make_choice function
     user_selection, bank, money_pot = make_choice(user_selection, bank, money_pot)
 
     if user_selection == 'b':
-        current_turn += 1
-        money_pot = 0
-        num_die_to_roll = 0
-        return num_die_to_roll, money_pot, bank, current_turn
-    
-    user_selection = [int(char) for char in user_selection]
-    
+        money_pot = 0   #reset money_pot
+        num_die_to_roll = 0  #reset num_die_to_roll
+        return num_die_to_roll, money_pot, bank, current_turn 
+
+    user_selection = validate_user_input(user_selection)
+
     user_selection, die_rolled = remove_from_die_rolled(user_selection, die_rolled)
     
     num_die_to_roll -= len(user_selection)
@@ -108,14 +83,54 @@ def make_choice(user_selection, bank, money_pot):
 
     else:
         points_earned_this_round = int(input("\nHow many points for this roll? "))
+        # Validate that input is an integer before adding to money_pot
         money_pot += points_earned_this_round
 
         return user_selection, bank, money_pot
 
-def bank_round(money_pot, bank):
+def validate_user_input(user_selection):
+    #Fix to allow user to also enter with spaces 
+    user_selection = [int(char) for char in user_selection]
+    return user_selection
+
+def game_summary(bank, money_pot, current_turn, max_turns):
+    print('\n***********************')
+    print(f'BANK VALUE        {bank}')
+    print(f'MONEY POT VALUE   {money_pot}')
+    print(f'CURRENT TURN      {current_turn}/{max_turns}.')
+    print('***********************\n')
+    time.sleep(.3)
+
+def rolling_die_text():
+    print("rolling die...\n")
+    time.sleep(.3)
+
+def determine_which_die_rolled(die_rolled, num_die_to_roll):
+    for i in range(num_die_to_roll):
+        die_rolled.append(random.randint(min_die_val, max_die_val))
+    
+    print(f'You rolled: {die_rolled}\n')
+    return die_rolled, num_die_to_roll
+
+def bank_and_reset_die(money_pot, bank, current_turn):
+    print('All die used\n')
+    print('Points banked and die reset\n')
+
+    time.sleep(.5)
+    current_turn += 1
+    print('New turn started\n')
+    
+    return money_pot, bank, current_turn
+
+def bank_round(money_pot, bank, current_turn):
     bank += money_pot
     money_pot = 0
     return money_pot, bank 
+
+def end_game(bank):
+    print("Out of turns.\n")
+    print(f'Game over. You scored {bank} points!')
+    quit()
 
 def main():
     welcome()
